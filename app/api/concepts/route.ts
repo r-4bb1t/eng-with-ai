85ece810d8a5577714913e0b80a0cce66da55454
r4bb1t <r4bb1t.github@gmail.com>
2024-04-07 10:01:30 +0900

    test: add console.log

--- app/api/concepts/route.ts
+++ app/api/concepts/route.ts
@@ -1,5 +1,7 @@
 import { getRandomConcept } from "@/app/constants/concepts";
 
 export const GET = async (_: Request) => {
-  return Response.json({ concept: getRandomConcept() });
+  const concept = getRandomConcept();
+  console.log(concept);
+  return Response.json({ concept });
 };
